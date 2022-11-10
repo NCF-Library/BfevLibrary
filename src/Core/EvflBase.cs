f0b22f7972a9a6ef454423c0a263d59e00181bd8
ArchLeaders <archparliament@gmail.com>
2022-11-10 14:24:15 -0800

    Parse BFEVFL header

--- src/Core/EvflBase.cs
+++ src/Core/EvflBase.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Buffers.Binary;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,31 +12,56 @@ namespace EvflLibrary.Core
 {
     public class EvflBase
     {
-        //
-        // Offsets
+        public string Magic = "BFEVFL\x00\x00";
+        public byte VersionMajor;
+        public byte VersionMinor;
+        public byte VersionPatch;
+        public byte VersionSubPatch;
+        public short ByteOrder;
+        public byte Alignment;
+        public int FileNameOffset;
+        public ushort IsRelocatedFlag;
+        public ushort FirstBlockOffset;
+        public int RelocationTableOffset;
+        public int FileSize;
+        public ushort FlowChartCount;
+        public ushort TimelineCount;
+        public ulong FlowchartOffsetPtr;
+        public ulong FlowchartNameDictionaryOffset;
+        public ulong TimelineOffsetPtr;
+        public ulong TimelineNameDictionaryOffset;
 
+        public void Read(string file)
+        {
+            using FileStream fs = File.OpenRead(file);
+            using BinaryReader reader = new(fs);
 
-        //
-        // Meta data
+            Magic = new(reader.ReadChars(8));
+            VersionMajor = reader.ReadByte();
+            VersionMinor = reader.ReadByte();
+            VersionPatch = reader.ReadByte();
+            VersionSubPatch = reader.ReadByte();
+            ByteOrder = reader.ReadInt16();
+            Alignment = reader.ReadByte();
+            fs.Position += 1; // Padding
+            FileNameOffset = reader.ReadInt32();
+            IsRelocatedFlag = reader.ReadUInt16();
+            FirstBlockOffset = reader.ReadUInt16();
+            RelocationTableOffset = reader.ReadInt32();
+            FileSize = reader.ReadInt32();
+            FlowChartCount = reader.ReadUInt16();
+            TimelineCount = reader.ReadUInt16();
+            fs.Position += 4; // Padding
+            FlowchartOffsetPtr = reader.ReadUInt64();
+            FlowchartNameDictionaryOffset = reader.ReadUInt64();
+            TimelineOffsetPtr = reader.ReadUInt64();
+            TimelineNameDictionaryOffset = reader.ReadUInt64();
 
-        internal string Magic = "BFEVFL\x00\x00";
-        internal byte VersionMajor;
-        internal byte VersionMinor;
-        internal byte VersionPatch;
-        internal byte VersionSubPatch;
-        internal short ByteOrder;
-        internal byte Padding1;
-        internal int FileNameOffset;
-        internal ushort IsRelocatedFlag;
-        internal ushort FirstBlockOffset;
-        internal ushort RelocationTableOffset;
-        internal int FileSize;
-        internal ushort FlowChartCount;
-        internal ushort TimelineCount;
-        internal uint Padding2;
-        internal dynamic? FlowchartPtr;
-        internal dynamic? FlowchartNameDictionary;
-        internal dynamic? TimelinePtr;
-        internal dynamic? TimelineNameDictionary;
+            var fields = GetType().GetFields();
+            foreach (var field in fields)
+            {
+                Debug.WriteLine($"{field.Name}: {field.GetValue(this) ?? "null"}");
+            }
+        }
     }
 }
