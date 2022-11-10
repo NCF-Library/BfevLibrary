6f280ded9476b0079c883e2774ac835d73c3c83a
ArchLeaders <archparliament@gmail.com>
2022-11-10 14:24:59 -0800

    Add unit tests project

---
+++ tests/ReadEvFl.cs
@@ -0,0 +1,20 @@
+﻿using EvflLibrary.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass]
+    public class ReadEvFl
+    {
+        [TestMethod]
+        public void Read()
+        {
+            EvflBase evfl = new();
+            evfl.Read(".\\Data\\100enemy.bfevfl");
+        }
+    }
+}
