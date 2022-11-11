d42cd91cf75b014b0548085b2e6b104a849a7707
ArchLeaders <archparliament@gmail.com>
2022-11-11 10:39:53 -0800

    Create binary reader extensions

---
+++ src/Extensions/BinaryReaderExtension.cs
@@ -0,0 +1,32 @@
+﻿using System.Xml.Linq;
+
+namespace EvflLibrary.Extensions
+{
+    public static class BinaryReaderExtension
+    {
+        public static string ReadStringPtr(this BinaryReader reader)
+        {
+            return new(reader.TemporarySeek(reader.ReadInt64(), SeekOrigin.Begin, () => {
+                ushort size = reader.ReadUInt16();
+                return reader.ReadChars(size);
+            }));
+        }
+
+        public static void TemporarySeek(this BinaryReader reader, long position, SeekOrigin origin, Action read)
+        {
+            long initPos = reader.BaseStream.Position;
+            reader.BaseStream.Seek(position, origin);
+            read();
+            reader.BaseStream.Position = initPos;
+        }
+
+        public static T TemporarySeek<T>(this BinaryReader reader, long position, SeekOrigin origin, Func<T> read)
+        {
+            long initPos = reader.BaseStream.Position;
+            reader.BaseStream.Seek(position, origin);
+            T readResults = read();
+            reader.BaseStream.Position = initPos;
+            return readResults;
+        }
+    }
+}
