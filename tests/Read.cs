e0aebf188635a21d6d1a614ef27df621ed20d977
ArchLeaders <archparliament@gmail.com>
2022-12-14 20:30:18 -0800

    Extend Evfl read tests

--- tests/Read.cs
+++ tests/Read.cs
@@ -1,11 +1,12 @@
 ﻿using EvflLibrary.Core;
 using Microsoft.VisualStudio.TestTools.UnitTesting.Logging;
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Tests
@@ -17,17 +18,23 @@ namespace Tests
         public void ReadEvFl()
         {
             EvflBase evfl = new(".\\Data\\100enemy.bfevfl");
-            string serialized = JsonConvert.SerializeObject(evfl, new JsonSerializerSettings() { Formatting = Formatting.Indented, NullValueHandling = NullValueHandling.Ignore });
-            // Debug.WriteLine(serialized);
-            File.WriteAllText(".\\100enemy.json", serialized);
+            string serialized = JsonSerializer.Serialize(evfl, new JsonSerializerOptions() {
+                WriteIndented = true,
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+            });
+            Debug.WriteLine(serialized);
+            File.WriteAllText(".\\Data\\100enemy.json", serialized);
         }
 
         [TestMethod]
         public void ReadEvTm()
         {
-            EvflBase evfl = new(".\\Data\\Demo161_0.bfevtm");
-            string serialized = JsonConvert.SerializeObject(evfl, new JsonSerializerSettings() { Formatting = Formatting.Indented, NullValueHandling = NullValueHandling.Ignore });
-            // Debug.WriteLine(serialized);
+            EvflBase evtm = new(".\\Data\\Demo161_0.bfevtm");
+            string serialized = JsonSerializer.Serialize(evtm, new JsonSerializerOptions() {
+                WriteIndented = true,
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+            });
+            Debug.WriteLine(serialized);
             File.WriteAllText(".\\Demo161_0.json", serialized);
         }
     }
