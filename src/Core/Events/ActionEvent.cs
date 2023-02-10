30a7226c02c9736f8425fcf6798d640aceb37f65
ArchLeaders <archparliament@gmail.com>
2023-02-09 20:11:05 -0800

    Add ActorAction for clarity and ease-of-use

--- src/Core/Events/ActionEvent.cs
+++ src/Core/Events/ActionEvent.cs
@@ -18,6 +18,7 @@ namespace BfevLibrary.Core
         [JsonIgnore]
         public Actor? Actor => ActorIndex > -1 ? _parent?.Actors[ActorIndex] : null;
         public string? ActorName => Actor?.Name;
+        public string? ActorAction => ActorActionIndex < -1 ? Actor?.Actions[ActorActionIndex] : null;
 
         [JsonConstructor]
         public ActionEvent(string name) : base(name, EventType.Action)
