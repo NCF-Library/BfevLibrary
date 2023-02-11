16cd43ac8b0fc38770478a0eb6e5142ee82a201c
ArchLeaders <archparliament@gmail.com>
2023-02-10 17:43:50 -0800

    Add debug program

---
+++ tests/Program.cs
@@ -0,0 +1,14 @@
+﻿using BfevLibrary;
+using BfevLibrary.Core;
+using BfevLibrary.Core.Collections;
+
+BfevFile evfl = BfevFile.FromBinary(@"D:\Bin\AutoRRG\Randomizer_GoalMode.bfevfl");
+
+Flowchart flowchart = evfl.Flowchart!;
+EventList events = flowchart.Events;
+
+events.RemoveAt(flowchart.EntryPoints["RandomGoal_Choose_no"].EventIndex, recursive: true);
+// evfl.Flowchart!.EntryPoints.Remove("RandomGoal_Choose_no");
+
+byte[] data = evfl.ToBinary();
+File.WriteAllBytes("D:\\Bin\\AutoRRG\\new.bfevfl", data);
