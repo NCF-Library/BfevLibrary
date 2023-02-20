08bdeb7041a282ce2e819dff6a44435e9386468a
ArchLeaders <archparliament@gmail.com>
2023-02-19 22:12:55 -0800

    Update tests

--- tests/Program.cs
+++ tests/Program.cs
@@ -1,14 +1,7 @@
 ﻿using BfevLibrary;
-using BfevLibrary.Core;
-using BfevLibrary.Core.Collections;
 
-BfevFile evfl = BfevFile.FromBinary(@"D:\Bin\AutoRRG\Randomizer_GoalMode.bfevfl");
-
-Flowchart flowchart = evfl.Flowchart!;
-EventList events = flowchart.Events;
-
-events.RemoveAt(flowchart.EntryPoints["RandomGoal_Choose_no"].EventIndex, recursive: true);
-evfl.Flowchart!.EntryPoints.Remove("RandomGoal_Choose_no");
+BfevFile evfl = BfevFile.FromBinary(@"D:\Bin\AutoRRG\ShopForRandomGoal.bfevfl");
+evfl.Flowchart!.RemoveEntryPoint("Yorozuya_Kaitori");
 
 byte[] data = evfl.ToBinary();
 File.WriteAllBytes("D:\\Bin\\AutoRRG\\new.bfevfl", data);
